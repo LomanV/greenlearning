3a0f2caf8e6a3d1a9b426b7f7936993284075688
LomanV <47893263+LomanV@users.noreply.github.com>
2023-08-24 17:00:54 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,4 @@
-from network import Net
-
+import scipy
 import scipy.io
 import numpy as np
 import torch
@@ -29,3 +28,22 @@ def load_data(filename):
     eval_G = torch.concat((xG,yG), 1).float()
 
     return x, y, u, f, eval_G
+
+def load_data(filename, device, type):
+    data_idn = scipy.io.loadmat(filename)
+
+    u = torch.tensor(data_idn['U'], dtype=torch.float32)
+    f = torch.tensor(data_idn['F'], dtype=torch.float32)
+
+    u_train, u_test = u[:,0:100].to(device), u[:,100:200]
+    f_train, f_test = f[:,0:100].to(device), f[:,100:200]
+
+    if (type=='2d' or type=='time'):
+        fx = torch.tensor(data_idn['fX'], dtype=torch.float32)
+        return u_train, u_test, f_train, f_test, fx
+    elif type=='1d':
+        x = torch.tensor(data_idn['X']).float()
+        y = torch.tensor(data_idn['Y']).float()
+        return u_train, u_test, f_train, f_test, x, y
+    else:
+        return
