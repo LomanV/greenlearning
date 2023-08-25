bc91e59c22f8e029281de1cf694bbf759762d532
LomanV <47893263+LomanV@users.noreply.github.com>
2023-08-25 12:00:57 +0200

    Update train_time.py

--- time_analysis/train_time.py
+++ time_analysis/train_time.py
@@ -10,19 +10,32 @@ import numpy as np
 from tqdm import tqdm, trange
 import copy
 
-n_epochs  = 10**4
-G     = Net(3, 1, 8, 20).to(device)
+# Set device, GPU is highly recommended
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Initialise network
+G = Net(3, 1, 8, 20).to(device)
 G.apply(init_weights)
+
+# Define loss function
 loss_func = GreenLossTime()
 
 # define the Adam optimizer and pass the combined parameters to it
 optimizer = optim.Adam(G.parameters(), lr=0.001)
 
+# Save loss profile and best model
 history = np.zeros(n_epochs)
 best_loss = 100
 
+# Load data
+eq       = "heat" # choose from heat wave_x wave_t wave_x is wave with spatial boundary condition, wave_t time boundary conditions
+forcing  = "cheb" # choose from cheb sine pwl gaus
+filename = "dataset/" + eq + "_" + forcing + ".mat"
+
+u_train, u_test, f_train, f_test, fx = load_data(filename, device, 'time')
 eval_G = eval_set_time(fx, device)
 
+n_epochs  = 10**4
 with trange(n_epochs, unit='epochs') as pbar:
     for epoch in pbar:
 
