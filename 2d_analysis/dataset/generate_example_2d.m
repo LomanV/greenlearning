bfeccc787d0c88523d0318427b3be5db5c556d82
LomanV <47893263+LomanV@users.noreply.github.com>
2023-09-06 11:24:05 +0200

    Update generate_example_2d.m

--- 2d_analysis/dataset/generate_example_2d.m
+++ 2d_analysis/dataset/generate_example_2d.m
@@ -8,7 +8,7 @@ function generate_example_2d(varargin)
     % Choose forcing type from pwl (continuous piecewise-linear) sine
     % cheb (Chebyshev polynomials) or gaus (drawn from Gaussian process)
     forcing = "gaus";
-    points  = 100;
+    points  = 40;
 
     % Definition of a square domain [0, 1]^2
     dom  = [0 1];
@@ -226,4 +226,44 @@ function z = rand_gaus(L, x, y)
 
     z = f(x).*g(y);
 
+end
+
+% ChebyshevPoly.m by David Terr, Raytheon, 5-10-04
+
+% Given nonnegative integer n, compute the 
+% Chebyshev polynomial T_n. Return the result as a column vector whose mth
+% element is the coefficient of x^(n+1-m).
+% polyval(ChebyshevPoly(n),x) evaluates T_n(x).
+
+function tk = ChebyshevPoly(n)
+
+    if n==0 
+        tk = 1;
+    elseif n==1
+        tk = [1 0]';
+    else
+        
+        tkm2 = zeros(n+1,1);
+        tkm2(n+1) = 1;
+        tkm1 = zeros(n+1,1);
+        tkm1(n) = 1;
+    
+        for k=2:n
+            
+            tk = zeros(n+1,1);
+    
+            for e=n-k+1:2:n
+                tk(e) = 2*tkm1(e+1) - tkm2(e);
+            end
+            
+            if mod(k,2)==0
+                tk(n+1) = (-1)^(k/2);
+            end
+            
+            if k<n
+                tkm2 = tkm1;
+                tkm1 = tk;
+            end 
+        end
+    end
 end
